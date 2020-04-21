42c72d88faca06c59cc3df2ebce12e8f8cbe9990
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-21 22:18:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 def load_library(file)
   hash = YAML.load_file(file) # Use load_file to get a hash (meaning) of arrays (emote_array)
   hash.each_pair do |meaning, emote_array|
-    hash[meaning] = {:english => emote_array[0], :japanese => emote_array[1] }
+    hash[meaning] = { :english => emote_array[0], :japanese => emote_array[1] }
   end
 end
 
