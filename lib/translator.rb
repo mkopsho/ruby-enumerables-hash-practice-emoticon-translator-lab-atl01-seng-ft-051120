3a61f9d3293cea8e020916d781d1353c644e1cee
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-21 22:35:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,13 +10,13 @@ end
 def get_japanese_emoticon(file, icon)
   hash = load_library(file) # Use our previous method to load the yaml file.
   hash.each_pair do |meaning, languages| # Use each_pair enumerable to give us access to the first nested hash that we just made.
-    languages.each_pair do |emote| # Use each_pair enumerable to give us access to the arrays of emoticons.
+    languages.each_pair do |emote| # Use each_pair enumerable to give us access to the language/emoticon pairs.
       if icon == emote[1]
-        return languages[:japanese] # If the icon parameter matches return the equivalent Japanese emote.
+        return languages[:japanese] # If the icon parameter matches the current emoticon, return the equivalent Japanese emoticon.
       end
     end
   end
-  return "Sorry, that emoticon was not found" # If nothing returned, we didn't find that emote.
+  return "Sorry, that emoticon was not found" # If nothing returned from our loop, we didn't find that emote.
 end
 
 def get_english_meaning(file, icon)
@@ -24,9 +24,9 @@ def get_english_meaning(file, icon)
   hash.each_pair do |meaning, languages|
     languages.each_pair do |emote|
       if icon == emote[1]
-        return meaning
+        return meaning # The logic is the exact same as the above method, except we return the assigned key (`meaning`) if the icon parameter matches the current emoticon.
       end
     end
   end
-  return "Sorry, that emoticon was not found"
+  return "Sorry, that emoticon was not found" # If nothing returned from our loop, we didn't find that emote.
 end
