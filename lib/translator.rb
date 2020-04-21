2d5b4c23fdef97ca46328e65a29e174d6e929153
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-21 22:04:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file)
+  hash = YAML.load_file(file) # Use load_file to get a hash (meaning) of arrays (emote_array)
+  hash.each_pair do |meaning, emote_array|
+    hash[meaning] = {:english => emote_array[0], :japanese => emote_array[1] }
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+# def get_japanese_emoticon(file, icon)
+#   load_library(file)
+# end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, icon)
+  hash = load_library(file)
+  hash.each_pair do |meaning, languages|
+    languages.each_pair do |emote|
+      if icon == emote[1]
+        return meaning
+      end
+    end
+  end
 end
