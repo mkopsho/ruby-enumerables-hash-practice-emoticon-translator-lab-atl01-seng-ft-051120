c6968152a95b6bfc36bbcff60e12c34d68761fc9
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-21 22:22:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,16 +1,16 @@
 require "yaml"
 
 def load_library(file)
-  hash = YAML.load_file(file) # Use load_file to get an initial hash of arrays
+  hash = YAML.load_file(file) # Use load_file to get an initial hash of arrays.
   hash.each_pair do |meaning, emote_array|
-    hash[meaning] = { :english => emote_array[0], :japanese => emote_array[1] } # For each top-level key (meaning), assign a hash of arrays with :english, :japanese languages as the keys
+    hash[meaning] = { :english => emote_array[0], :japanese => emote_array[1] } # For each top-level key (meaning), assign a hash of arrays with :english, :japanese languages as the keys.
   end
 end
 
 def get_japanese_emoticon(file, icon)
-  hash = load_library(file) # Use our previous method to load the yaml file
-  hash.each_pair do |meaning, languages| # Use each_pair enumerable to give us access to 
-    languages.each_pair do |emote|
+  hash = load_library(file) # Use our previous method to load the yaml file.
+  hash.each_pair do |meaning, languages| # Use each_pair enumerable to give us access to the first nested hash that we just made.
+    languages.each_pair do |emote| # Use each_pair enumerable to give us access to 
       if icon == emote[1]
         return languages[:japanese]
       end
